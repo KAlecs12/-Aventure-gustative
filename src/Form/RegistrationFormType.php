034f036c17c3edf6b20fe5bd79aed5bfc9731ca0
Anthony <seaflyscontact@gmail.com>
2022-05-30 21:23:33 +0200

    :art: Add Register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,10 +18,22 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
+            ->add('pseudo', TextType::class, [
+            'label' => false,
+
+            ])
+            ->add('firstName', TextType::class, [
+            'label' => false,
+
+            ])
+            ->add('lastName', TextType::class, [
+            'label' => false,
+
+            ])
+            ->add('email', TextType::class, [
+            'label' => false,
+
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -30,6 +43,7 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
+
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
